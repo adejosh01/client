fccd077699388008edc76c8b58fdee768717bbe1
Adeloba Joshua <adelobajoshua19@gmail.com>
2024-03-09 23:55:50 +0100

    redirect

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-2g7yotyaj1vhxpl0.us.auth0.com"
       clientId="WqMfP41RSa4iiyikYJyMb2ytjMo3Dodf"
       authorizationParams={{
-        redirect_uri: "https://homyz-steel.vercel.app/"
+        redirect_uri: "http://localhost:5173"
       }}
       audience="http://localhost:8008"
       scope="openid profile email"
